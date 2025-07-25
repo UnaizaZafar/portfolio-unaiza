a9f2b34054446ef3896638e0f2b80d4771a9356a
Unaiza08 <i.unaiza.aginfotech@gmail.com>
2025-07-25 18:04:30 +0500

    created about me section

---
+++ app/components/AboutMe.jsx
@@ -0,0 +1,21 @@
+
+const AboutMe = () => {
+    return (
+        <div
+            className=" h-screen  flex justify-between items-center bg-primary mx-auto px-32">
+            <div className="grid grid-cols-3 gap-3">
+                <p className="font-gamilia text-[100px] text-secondary">Hi! I'm Unaiza Zafar</p>
+                <p className="text-2xl/loose col-span-2 text-secondary">I’m a 23-year-old frontend developer who’s been working professionally for nearly a year.
+                    My journey with technology began as curiosity soon turned into a passion for crafting beautiful, responsive web experiences.
+                    <br />
+                    Currently working as a frontend developer, I focus on building clean, structured, and user-focused interfaces using tools like React, Next.js, and Tailwind CSS. I value maintainable code, clear structure, and strong logic. I’m quick to pick up new technologies and love solving challenges with a curious mindset.
+                    <br />
+                    This journey hasn’t been perfect — I’ve made mistakes, and I know I’ll make more. But I’ve learned that mistakes are just part of the process. What matters is the growth that comes from them, and the love for what I do that keeps me moving forward.
+
+                    Stick around — I’ll be sharing my growth, projects, and learnings as I continue building in public.</p>
+            </div>
+        </div>
+    )
+}
+
+export default AboutMe
---
+++ app/components/HeroSection.jsx
@@ -0,0 +1,75 @@
+"use client";
+import Link from "next/link";
+import { useRef } from "react";
+import TextAnimations from "./TextAnimations";
+const HeroSection = () => {
+    const wordsRef = useRef();
+    const lineRef = useRef([]);
+    TextAnimations({ wordAnimation: wordsRef, LineAnimation: lineRef });
+
+    return (
+        <section id="hero" className="bg-secondary h-screen flex flex-col gap-6 items-center justify-center">
+            <h1
+                ref={wordsRef}
+                className="font-gamilia text-[160px] font-medium leading-32 text-center text-primary max-w-3/5 "
+            >
+                Design meets development in my little corner of the web
+            </h1>
+
+            <p
+                ref={(el) => (lineRef.current[0] = el)}
+                className="text-xl font-cobe opacity-0"
+            >
+                I create immersive digital experiences with code and creativity.
+                {/* I'm Unaiza, a frontend developer building elegant web experiences. */}
+            </p>
+            <Link
+                ref={(el) => (lineRef.current[1] = el)}
+                download={"UnaizaZ-Resume"}
+                href="/Unaiza Zafar Resume.pdf"
+                className="opacity-0 download-button font-cobe transition-all duration-300 ease-in-out"
+            >
+                <div className="docs">
+                    {/* File icon */}
+                    <svg
+                        viewBox="0 0 24 24"
+                        width={24}
+                        height={24}
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="css-i6dzq1"
+                    >
+                        <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z" />
+                        <polyline points="14 2 14 8 20 8" />
+                        <line x1={16} y1={13} x2={8} y2={13} />
+                        <line x1={16} y1={17} x2={8} y2={17} />
+                        <polyline points="10 9 9 9 8 9" />
+                    </svg>
+                    Download CV
+                </div>
+                <div className="download">
+                    <svg
+                        viewBox="0 0 24 24"
+                        width={24}
+                        height={24}
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        className="css-i6dzq1"
+                    >
+                        <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+                        <polyline points="7 10 12 15 17 10" />
+                        <line x1={12} y1={15} x2={12} y2={3} />
+                    </svg>
+                </div>
+            </Link>
+        </section>
+    )
+}
+
+export default HeroSection
