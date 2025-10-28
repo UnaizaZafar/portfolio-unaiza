32f5f50e9dcb18609987039f97ddf33bce8d4bd1
UnaizaZafar <unaizaofficial840@gmail.com>
2025-10-28 19:25:50 +0500

    responsives added to about section

--- app/components/AboutMe.jsx
+++ app/components/AboutMe.jsx
@@ -41,12 +41,12 @@ const AboutMe = () => {
         <div id="about"
             className="flex justify-between items-center mx-auto px-6 py-8 xl:px-32">
             <div className="flex flex-col max-w-screen-lg mx-auto rounded-3xl border-3 border-purple-900/80 overflow-hidden">
-                <div className="flex gap-2.5 px-6 py-3 bg-purple-900/80">
+                <div className="flex gap-2.5 px-2 lg:px-6 py-3 bg-purple-900/80">
                     <div className="rounded-full size-3.5 bg-rose-400" />
                     <div className="rounded-full size-3.5 bg-yellow-400" />
                     <div className="rounded-full size-3.5 bg-emerald-600" />
                 </div>
-                <div className="flex flex-col gap-5 bg-gradient-to-b from-purple-950/70 via-purple-950 to-purple-900/85 shadow-inner px-6 py-10">
+                <div className="flex flex-col gap-3 lg:gap-5 bg-gradient-to-b from-purple-950/70 via-purple-950 to-purple-900/85 shadow-inner px-2 lg:px-6 py-5 lg:py-10">
                     <p ref={fadeDownwardRef} className="font-gamilia text-5xl sm:text-6xl md:text-7xl 2xl:text-8xl text-white">Hi! I'm Unaiza Zafar</p>
                     <p ref={fadeUpwardRef} className="text-lg text-white">
                         I’m a 23-year-old Frontend Developer with <strong>1+ year of professional experience and over 12 completed projects</strong>. What started as curiosity quickly grew into a passion for crafting clean, responsive, and user-focused web experiences.
