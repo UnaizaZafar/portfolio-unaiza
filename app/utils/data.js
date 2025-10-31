912773375341179098b04eabf139a5dfe33e70ce
UnaizaZafar <unaizaofficial840@gmail.com>
2025-10-31 10:23:15 +0500

    removed portfolio section from landing page and menu

--- app/utils/data.js
+++ app/utils/data.js
@@ -22,53 +22,53 @@ export const menu = [
       </svg>
     ),
   },
-  {
-    id: 2,
-    name: "Experience",
-    link: "#work-experience",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width={35}
-        height={35}
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth={2}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="icon icon-tabler icons-tabler-outline icon-tabler-briefcase-2 size-7"
-      >
-        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-        <path d="M3 9a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-9z" />
-        <path d="M8 7v-2a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v2" />
-      </svg>
-    ),
-  },
-  {
-    id: 3,
-    name: "Portfolio ",
-    link: "#portfolio",
-    icon: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width={35}
-        height={35}
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth={2}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="icon icon-tabler icons-tabler-outline icon-tabler-file-smile size-7"
-      >
-        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-        <path d="M14 3v4a1 1 0 0 0 1 1h4" />
-        <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2zm-7 -7h.01m3.99 0h.01" />
-        <path d="M10 17a3.5 3.5 0 0 0 4 0" />
-      </svg>
-    ),
-  },
+  // {
+  //   id: 2,
+  //   name: "Experience",
+  //   link: "#work-experience",
+  //   icon: (
+  //     <svg
+  //       xmlns="http://www.w3.org/2000/svg"
+  //       width={35}
+  //       height={35}
+  //       viewBox="0 0 24 24"
+  //       fill="none"
+  //       stroke="currentColor"
+  //       strokeWidth={2}
+  //       strokeLinecap="round"
+  //       strokeLinejoin="round"
+  //       className="icon icon-tabler icons-tabler-outline icon-tabler-briefcase-2 size-7"
+  //     >
+  //       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+  //       <path d="M3 9a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-9z" />
+  //       <path d="M8 7v-2a2 2 0 0 1 2 -2h4a2 2 0 0 1 2 2v2" />
+  //     </svg>
+  //   ),
+  // },
+  // {
+  //   id: 3,
+  //   name: "Portfolio ",
+  //   link: "#portfolio",
+  //   icon: (
+  //     <svg
+  //       xmlns="http://www.w3.org/2000/svg"
+  //       width={35}
+  //       height={35}
+  //       viewBox="0 0 24 24"
+  //       fill="none"
+  //       stroke="currentColor"
+  //       strokeWidth={2}
+  //       strokeLinecap="round"
+  //       strokeLinejoin="round"
+  //       className="icon icon-tabler icons-tabler-outline icon-tabler-file-smile size-7"
+  //     >
+  //       <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+  //       <path d="M14 3v4a1 1 0 0 0 1 1h4" />
+  //       <path d="M17 21h-10a2 2 0 0 1 -2 -2v-14a2 2 0 0 1 2 -2h7l5 5v11a2 2 0 0 1 -2 2zm-7 -7h.01m3.99 0h.01" />
+  //       <path d="M10 17a3.5 3.5 0 0 0 4 0" />
+  //     </svg>
+  //   ),
+  // },
   {
     id: 4,
     name: "SkillSet",
